2ccf954543a4333fda63ad93f2acfe8d990439a0
Leandro Huber <leandrohuber@outlook.com>
2026-02-05 21:46:51 +0000

    Remove: vueDevTools from production

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,17 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import vueDevTools from "vite-plugin-vue-devtools";
 
 export default defineConfig({
-    plugins: [vue(), vueDevTools()],
+    plugins: [vue()],
     resolve: {
         alias: {
             "@": fileURLToPath(new URL("./src", import.meta.url)),
         },
     },
     server: {
-        allowedHosts: ["forecast.leandrohuber.com.br", ".leandrohuber.com.br"],
-    },
+        host: '0.0.0.0',
+        port: 5173,
+        allowedHosts: ['forecast.leandrohuber.com.br', '.leandrohuber.com.br', 'localhost', '127.0.0.1']
+    }
 });
